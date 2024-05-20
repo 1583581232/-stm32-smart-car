41f7e9cfcc68141be2a1c0c823b5173e7910c52e
GodLeaveMe <119564588+1583581232@users.noreply.github.com>
2024-05-20 15:14:18 +0800

    Add files via upload

---
+++ Objects/motor.d
@@ -0,0 +1,15 @@
+./objects/motor.o: hardware\motor.c hardware\motor.h system\sys.h \
+  D:\Keil_v5\Keil\STM32F1xx_DFP\1.0.5\Device\Include\stm32f10x.h \
+  D:\Keil_v5\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\Keil_v5\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  D:\Keil_v5\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  D:\Keil_v5\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  D:\Keil_v5\Keil\STM32F1xx_DFP\1.0.5\Device\Include\system_stm32f10x.h \
+  RTE\Device\STM32F103ZE\stm32f10x_conf.h RTE\_user\RTE_Components.h \
+  D:\Keil_v5\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_exti.h \
+  D:\Keil_v5\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h \
+  D:\Keil_v5\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h \
+  D:\Keil_v5\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_tim.h \
+  D:\Keil_v5\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_usart.h \
+  D:\Keil_v5\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\misc.h
